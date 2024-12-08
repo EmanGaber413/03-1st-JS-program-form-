4f4c4f6a5d5b6ff99022f7bb198c947cce451cdd
Eman Gaber <emangaber413@gmail.com>
2024-12-08 23:25:13 +0200

    toast1

--- pro.js
+++ pro.js
@@ -1,10 +1,11 @@
-var weblist=[]
+var weblist = []
 var Websitename = document.getElementById("WebsiteName");
 var WebsiteLink = document.getElementById("WebsiteLink");
 var cardView = document.getElementById("cardView")
 var cardedit = document.getElementById("cardedit")
-var mainbtn=document.getElementById("mainbtn")
-var selectedindex=-1
+var mainbtn = document.getElementById("mainbtn")
+var toastElement = document.getElementById('bootstrap-toast');
+var selectedindex = -1
 
 
 // const toastTrigger = document.getElementById('liveToastBtn')
@@ -17,25 +18,28 @@ var selectedindex=-1
 //   })
 // }
 function mainbtnapp() {
-    
-if (mainbtn.innerHTML==="Save") {createnewlink()
-  
-}else{Update()}
-alert("added new link")
+
+  if (mainbtn.innerHTML === "Save") {
+    createnewlink()
+
+  } else { Update() }
+  // alert("added new link")
 }
 
-function createnewlink() { 
+function createnewlink() {
   // event.preventDefault();
-    var websiteObject =
-    {
-    name:Websitename.value,
-    link:WebsiteLink.value
-    };
-    weblist.push(websiteObject);
-    
+  var websiteObject =
+  {
+    name: Websitename.value,
+    link: WebsiteLink.value
+  };
+  weblist.push(websiteObject);
 
-    readwebsite();
-    clear();
+
+  readwebsite();
+  clear();
+  toast()
+
 }
 
 // console.log(weblist);
@@ -43,13 +47,13 @@ function createnewlink() {
 
 
 function readwebsite() {
-  var index=0
-  var HTMLCollectionwebsite=``;
-  
-    for (var item of weblist) {
-      
-        HTMLCollectionwebsite +=
-        ` <div class="col-sm-6 col-lg-4 ">
+  var index = 0
+  var HTMLCollectionwebsite = ``;
+
+  for (var item of weblist) {
+
+    HTMLCollectionwebsite +=
+      ` <div class="col-sm-6 col-lg-4 ">
         <div class="card bg-secondary-subtle ">
         <div class="card-body">
             <div class="col-12 row">
@@ -59,47 +63,49 @@ function readwebsite() {
             </div>
             <p class="card-text">${item.link} </p>
             <a href="http://${item.link}" target="_blank" class="btn btn-dark btn-outline-info">Visit Website</a>
-
-
-  
-            
           </div>
            </div>
-           </div>
+           </div>`
 
+    index++
+  }
 
+  cardView.innerHTML = HTMLCollectionwebsite;
+  toast()
 
-
-           
-           
-           
-           
-           `
-                
-            index++
-        }
-
-          cardView.innerHTML = HTMLCollectionwebsite ;
-    
 }
-function remove(index) 
-{ weblist.splice(index,1)
-  
+function remove(index) {
+  weblist.splice(index, 1)
+
   readwebsite()
-}
-function clear() {
+  function removetoast () {
+    
+ 
+  var toastDesign =
+      `<div class="position-fixed top-0 end-0 p-3 bg-danger text-white border-1 rounded-2" >
   
+    <strong class="me-auto text-capitalize">deleted link</strong>
+    </div>`;
+    toastElement.innerHTML = toastDesign
+  }
+  removetoast()
+}
 
-  Websitename.value=``
-  WebsiteLink.value=``
+function clear() {
+
+
+  Websitename.value = ``
+  WebsiteLink.value = ``
 }
 
 function edit(index) {
-  var editobject =weblist[index]
+  var editobject = weblist[index]
   Websitename.value = editobject.name
-   WebsiteLink.value =  editobject.link
-   mainbtn.innerHTML=("Edit")
-   selectedindex=index}
+  WebsiteLink.value = editobject.link
+  mainbtn.innerHTML = ("Edit")
+  selectedindex = index
+  toast()
+}
 
 function Update() {
 
@@ -107,44 +113,42 @@ function Update() {
   // Websitename.value = editobject.name
   //  WebsiteLink.value =  editobject.link
 
-   var newwebsiteObject =
-   {
-   name:Websitename.value,
-   link:WebsiteLink.value
-   };
-weblist.splice(selectedindex,1,newwebsiteObject)
-readwebsite()
-clear()
-mainbtn.innerHTML=("Save")
+  var newwebsiteObject =
+  {
+    name: Websitename.value,
+    link: WebsiteLink.value
+  };
+  weblist.splice(selectedindex, 1, newwebsiteObject)
+  readwebsite()
+  clear()
+  mainbtn.innerHTML = ("Save")
 }
 
 
-  //  htmledit =`<form class="row g-3">
-  //       <div class="col-md-12">
-          
-  //         <input type="email" class="form-control bg-dark-subtle" id="WebsiteLink" placeholder="https://someurl.com">${editobject.name} 
-  //       </div>
-  //       <div class="col-12 ">
-          
-  //         <input type="text" class="  form-control bg-dark-subtle "
-  //          id="WebsiteName" placeholder=" web site name"> ${editobject.link}
-           
-  //       </div>
-        
-        
-  //       <div class="col-12">
-        
-  //         <textarea type="textarea" style="resize: vertical; " rows="3" class="form-control bg-dark-subtle" id="inputAddress" 
-  //         placeholder="description">
-  //         </textarea>
-  //         </div>
-       
-  //       <div class="col-12">
-  //         <button type="button" class="btn btn-success col-12" onclick="createnewlink()">Create</button>
-  //       </div>
-  //     </form>`
 
-  //     cardedit.innerHTML = htmledit ;
+function toast() {
+  if (mainbtn.innerText === 'Save') {
+
+
+    var toastDesign =
+      `<div class="position-fixed top-0 end-0 p-3 bg-success text-white border-1 rounded-2" >
+  
+    <strong class="me-auto text-capitalize">Added new link</strong>
+    </div>`;
+    toastElement.innerHTML = toastDesign
+  }
+  else if (mainbtn.innerText === 'Edit') {
+    var toastDesign =
+    `<div class="position-fixed top-0 end-0 p-3 bg-warning text-white border-1 rounded-2" >
+  
+    <strong class="me-auto text-capitalize">Edit link</strong>
+    </div>`;
+    toastElement.innerHTML = toastDesign
+  }
+  }
+
+
+
 
 
 
